b3eac9898ce977dba18140539e63bf110e0681cf
atuki225 <atuki.prog@gmail.com>
2020-04-05 02:22:41 +0900

    コメントにユーザーネーム表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,7 @@ class CommentsController < ApplicationController
   def create
     @tweet = Tweet.find(params[:tweet_id])
     @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
     @comment.save
     redirect_to tweet_path(@tweet)
   end
