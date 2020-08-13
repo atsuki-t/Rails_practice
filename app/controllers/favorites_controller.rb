3606d0d7ffdc6cf95f36a42f5e8c1c6e47088b7b
atsuki-t <atsuki-t@weseek.co.jp>
2020-08-13 19:23:09 +0900

    戻し

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,10 +5,12 @@ class FavoritesController < ApplicationController
     # buildはnewと同じ意味で、アソシエーションしながらインスタンスをnewする時に形式的に使われる。
     favorite = current_user.favorites.build(tweet_id: params[:tweet_id])
     favorite.save
+    redirect_to tweets_path
   end
 
   def destroy
     favorite = Favorite.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
     favorite.destroy
+    redirect_to tweets_path
   end
 end
